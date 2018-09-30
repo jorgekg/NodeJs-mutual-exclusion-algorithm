c1be5a94ffc83bd3b6548f9f11350827a11cfca1
Jorge Guilherme Kohn <jorge.kohn@senior.com.br>
2018-09-30 15:42:03 -0300

    commit

--- backend/node/socket.js
+++ backend/node/socket.js
@@ -4,6 +4,7 @@ const stack = [];
 
 const wss = new WebSocketServer({port: 4206});
 LOOK = false;
+COORDENADOR = null;
 
 wss.on('connection', function(ws) {
   setInterval(function () {
@@ -14,41 +15,36 @@ wss.on('connection', function(ws) {
   }, 500);
   dead(ws);
   ws.on('message', function incoming(data) {
-    console.log(data);
     stack.push(data);
   });
 });
 
 function send(ws) {
-  const requestString = stack.shift();
-  if (requestString) {
-    const time = getRndInteger(5, 15) * 1000;
-    console.log(`levará ${time}`);
-    setInterval(() => {
-      console.log('Irá processar');
-      console.log(`Processando ${requestString}`);
-      if (requestString) {
-        const request = JSON.parse(requestString);
-        request.status = 1;
-        ws.send(JSON.stringify(request));
-        LOOK = false;
-        console.log('Concluido');
-      }
-    }, time);
+  if (stack.length > 0) {
+    const requestString = stack.shift();
+    if (requestString) {
+      const time = getRndInteger(5, 15) * 1000;
+      setInterval(() => {
+        if (requestString) {
+          const request = JSON.parse(requestString);
+          if (!COORDENADOR) {
+            COORDENADOR = request.id;
+            ws.send(JSON.stringify({coordenador: COORDENADOR}));
+          }
+          request.status = 1;
+          request.time = time;
+          ws.send(JSON.stringify(request));
+          LOOK = false;
+        }
+      }, time);
+    }
   }
 }
 
 function dead(ws) {
   setInterval(() => {
-    console.log(`Coordenador morreu`);
-    stack.forEach(requestString => {
-      const request = JSON.parse(requestString);
-      if (request.status === 0) {
-        request.status = 2;
-        ws.send(JSON.stringify(request));
-      }
-    });
-    stack = [];
+    COORDENADOR = null;
+    ws.send('dead');
   }, 60000);  
 }
 
--- frontend/requests/src/app/app.component.ts
+++ frontend/requests/src/app/app.component.ts
@@ -9,24 +9,37 @@ export class AppComponent implements OnInit {
   title = 'requests';
 
   public requestList = [];
+  public coordenador = null;
   private token = 1;
   private ws = new WebSocket('ws://localhost:4206/teste');
 
   ngOnInit() {
     const self = this;
     this.ws.onopen = function () {
-      self.requestList.push({id: self.requestList.length, status: 3, token: self.token});
+      self.requestList.push({id: self.requestList.length, status: 0, token: self.token, time: 0});
       self.requestList.forEach((request) => {
         self.ws.send(JSON.stringify(request));
       });
     };
     this.ws.onmessage = function(dt) {
-      const request = JSON.parse(dt.data);
-      self.requestList.forEach((req) => {
-        if (req.token === request.token) {
-          req.status = request.status;
-        }
-      });
+      if (dt.data === 'dead') {
+        self.requestList.forEach((req) => {
+          if (req.status !== 1) {
+            req.status = 2;
+          }
+        });
+        self.coordenador = 'Coordenador morreu';
+      } else if (dt.data.includes('coordenador')) {
+        self.coordenador = 'Requisição: ' + JSON.parse(dt.data).coordenador;
+      } else {
+        const request = JSON.parse(dt.data);
+        self.requestList.forEach((req) => {
+          if (req.token   === request.token && req.status !== 2) {
+            req.status = request.status;
+            req.time = request.time / 1000;
+          }
+        });
+      }
     };
     this.ws.onclose = function () {
       alert('close');
@@ -41,6 +54,7 @@ export class AppComponent implements OnInit {
       request.token = this.token;
       this.token++;
       request.status = 0;
+      request.time = 0;
       this.ws.send(JSON.stringify(request));
       this.send();
     }, this.getRndInteger(10, 25) * 1000);
@@ -48,9 +62,13 @@ export class AppComponent implements OnInit {
 
   private create() {
     setTimeout(() => {
-      this.requestList.push({id: this.requestList.length + 1, status: 0});
+      const request = {id: this.requestList.length, status: 0, token: this.token, time: 0};
+      this.token++;
+      request.status = 0;
+      this.requestList.push(request);
+      this.ws.send(JSON.stringify(request));
       this.create();
-    }, 10000);
+    }, 40000);
   }
 
   private getRndInteger(min, max) {
